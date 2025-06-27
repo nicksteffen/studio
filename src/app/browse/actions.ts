fffe4d4dfc3e6160ba7f5d6ef2c4e944469e4010
Nick Mafforti <nickmafforti@gmail.com>
2025-06-27 21:44:58 +0000

    so, I've confirmed that the issue does seem to be in the auth, but that

--- src/app/browse/actions.ts
+++ src/app/browse/actions.ts
@@ -32,12 +32,20 @@ export async function addItemFromCommunity(
         set(name: string, value: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value, ...options })
-          } catch (error) { /* The `set` method was called from a Server Component. */ }
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: '', ...options })
-          } catch (error) { /* The `delete` method was called from a Server Component. */ }
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     }
